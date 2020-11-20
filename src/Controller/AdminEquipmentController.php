fa0f3f1a975d1cf71d98e6d7d80d244f9ab8f014
Aurelie-F <aurelie.frarin@gmail.com>
2020-11-20 11:25:06 +0100

    US11 admin delete equipment

--- src/Controller/AdminEquipmentController.php
+++ src/Controller/AdminEquipmentController.php
@@ -81,7 +81,7 @@ class AdminEquipmentController extends AbstractController
         $equipments = $equipmentManager->selectAll();
 
         return $this->twig->render('Admin/equipmentList.html.twig', [
-            'equipments' => $equipments,
+            'equipments' => $equipments
         ]);
     }
 
@@ -89,7 +89,18 @@ class AdminEquipmentController extends AbstractController
     {
         $equipmentManager = new EquipmentManager();
         $equipment = $equipmentManager->selectOneById($id);
+        $isOrdered = $equipmentManager->isOrdered($id);
 
-        return $this->twig->render('Admin/equipmentDetail.html.twig', ['equipment' => $equipment]);
+        return $this->twig->render('Admin/equipmentDetail.html.twig', [
+            'equipment' => $equipment,
+            'isOrdered' => $isOrdered
+        ]);
+    }
+
+    public function delete(int $id)
+    {
+        $equipmentManager = new EquipmentManager();
+        $equipmentManager->delete($id);
+        header('Location:/adminEquipment/index');
     }
 }
