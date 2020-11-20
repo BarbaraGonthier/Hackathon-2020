4f66fb25bbaec8664ea6d94eb11871ad146b450a
AmelieRoussel <amelie.roussel41@gmail.com>
2020-11-20 11:36:45 +0100

    bug session ok

--- src/Controller/ExercisesController.php
+++ src/Controller/ExercisesController.php
@@ -104,6 +104,7 @@ class ExercisesController extends AbstractController
     {
         $exercisesManager = new ExercisesManager();
         $exercise = $exercisesManager->getExerciseById($id);
+        $_SESSION['done'] = [];
         if (key_exists($id, $_SESSION['done'])) {
             unset($_SESSION['done'][$id]);
         } else {
@@ -115,7 +116,7 @@ class ExercisesController extends AbstractController
     public function myExercises()
     {
         return $this->twig->render('Exercises/done.html.twig', [
-            'exercises' => $_SESSION['done']
+            'exercises' => $_SESSION['done'] ?? []
         ]);
     }
 }
